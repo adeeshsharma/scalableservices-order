22c02d368c712075bd74e589563de650e4a944d8
adeeshsharma <adeeshon@gmail.com>
2023-04-26 18:49:32 +0530

    implemented interservice comm

--- server.js
+++ server.js
@@ -3,14 +3,26 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const connect = require('./mongodb.js');
+const axios = require('axios');
 
 const swaggerInit = require('./swagger/swaggerInit.js');
 
 connect();
 
+const Product = mongoose.model('Product', {
+  name: String,
+  price: Number,
+});
+
+const User = mongoose.model('User', {
+  name: String,
+  email: String,
+  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+});
+
 const Order = mongoose.model('Order', {
-  userId: String,
-  productId: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
 });
 
 const app = express();
@@ -18,29 +30,47 @@ app.use(bodyParser.json());
 
 swaggerInit(app);
 
+// GET all orders and their products
 app.get('/orders', async (req, res) => {
   try {
-    const orders = await Order.find();
+    const orders = await Order.find().populate('products').populate('userId');
     res.json(orders);
   } catch (err) {
     console.log(err.message);
   }
 });
 
-app.get('/order/:idToGet', async (req, res) => {
+// GET a specific order by ID and its products
+app.get('/order/:id', async (req, res) => {
   try {
-    const id = req.params.idToGet;
-    const order = await Order.findById(id);
+    const id = req.params.id;
+    const order = await Order.findById(id)
+      .populate('products')
+      .populate('userId');
     res.json(order);
   } catch (err) {
     console.log(err.message);
   }
 });
 
+// GET all orders for a specific user
+app.get('/orders/user/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const orders = await Order.find({ userId })
+      .populate('products')
+      .populate('userId');
+    res.json(orders);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// POST a new order
 app.post('/order', async (req, res) => {
   try {
-    const { userId, productId } = req.body;
-    const order = new Order({ userId, productId });
+    const { userId, productIds } = req.body;
+    const order = new Order({ userId, products: productIds });
     await order.save();
     res.json(order);
   } catch (err) {
@@ -48,25 +78,11 @@ app.post('/order', async (req, res) => {
   }
 });
 
-app.put('/order/:idToUpdate', async (req, res) => {
+// DELETE an order by ID
+app.delete('/order/:id', async (req, res) => {
   try {
-    const { idToUpdate } = req.params;
-    const { userId, productId } = req.body;
-    const order = await Order.findByIdAndUpdate(
-      idToUpdate,
-      { userId, productId },
-      { new: true }
-    );
-    res.json(order);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
-
-app.delete('/order/:idToDelete', async (req, res) => {
-  try {
-    const { idToDelete } = req.params;
-    await Order.findByIdAndDelete(idToDelete);
+    const id = req.params.id;
+    await Order.findByIdAndDelete(id);
     res.sendStatus(204);
   } catch (err) {
     console.log(err.message);
